bb1713782d2bd73132d68cc5ff81f911c571bdba
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-06-12 13:48:19 +0200

    working reverse proxy

--- packages/main/src/App.js
+++ packages/main/src/App.js
@@ -6,17 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p><a href='/counter/'>React Showcase, TS, Redux</a></p>
+        <p><a href='/mobx/'>Mobx with TS</a></p>
       </header>
     </div>
   );

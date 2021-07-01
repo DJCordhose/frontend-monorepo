a9dade5d5cb8b42547a86414a08d4e28793b699f
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-07-01 08:42:15 +0200

    example of  how to just export certain parts of a whole folder

--- packages/zeigermann-component-lib/src/index.ts
+++ packages/zeigermann-component-lib/src/index.ts
@@ -2,3 +2,5 @@ export * from "./AriaButton"
 export * from "./FlexContainer"
 export * from "./LoadingIndicator"
 export * from "./util"
+// to show how to just export certain parts of a whole folder
+export {Table} from './table'
---
+++ packages/zeigermann-component-lib/src/table/index.ts
@@ -0,0 +1,2 @@
+// to show how to just export certain parts of a whole folder
+export * as Table from "./Table"
